b6850aed483950906b70b8921109320c0a7fc644
Sixten Tamleht <sixten.tamleht@elev.ga.ntig.se>
2025-12-17 11:07:16 +0100

    innan julen

--- Program.cs
+++ Program.cs
@@ -16,10 +16,22 @@ namespace Snake
 
 		public static bool display()
 		{
+			Console.Clear();
+			for(int y=0; y<4; ++y)
+			{
+				for(int x=0; x<4; ++x)
+				{
+					Console.Write(grid[x,y]);
+					Console.Write(' ');
+				}
+
+				Console.WriteLine();
+				Console.WriteLine();
+			}
 			return true;
 		}
 
-		public static void addBlock(bool canGive4, int chanseFor4 /*1 - 100, under or equal the variable it becomes a 4*/ ) 
+		public static void addBlock(bool canGive4/*orkade inte göra en overide*/, int chanseFor4 /*1 - 100, under or equal the variable it becomes a 4*/ ) 
 		{
 			int x;
 			int y;
@@ -35,16 +47,157 @@ namespace Snake
 				grid[x,y]=4;
 			else
 				grid[x,y]=2; 
-
 		}
 
-		public static void Main(string[] args)
+		public static char getKey()
 		{
+			ConsoleKeyInfo buttonPress;
+			buttonPress=Console.ReadKey();
+			if(buttonPress.Key == ConsoleKey.W)
+				return 'w';
+			if(buttonPress.Key == ConsoleKey.A)
+				return 'a';
+			if(buttonPress.Key == ConsoleKey.S)
+				return 's';
+			if(buttonPress.Key == ConsoleKey.D)
+				return 'd';
 
+			return '0';
+		}
+
+		static void MoveLogic()
+		{
+			int x=0, y=0;
+			int xDir=0, yDir=0;
+			char move=getKey();
+			if(move=='w')
+			{
+				yDir=-1;
+				y=3;
+			}
+			if(move=='s')
+			{
+				yDir=1;
+				y=0;
+			}
+			if(move=='a')
+			{
+				xDir=-1;
+				x=3;
+			}
+			if(move=='d')
+			{
+				xDir=1;
+				x=0;
+			}
+
+			int[] newLine = {0, 0, 0, 0};
+			int addedBlocks=0;
+			if(xDir!=0) 
+				for(; y<4; ++y)
+				{
+					for(int iX=0; iX<4; ++iX)
+						for(int i=iX-xDir; i+iX!=-1 && i+x!=4; i-=xDir)
+							if(grid[x+i,y]==grid[x,y] && grid[x,y]!=0)
+							{
+								newLine[addedBlocks++]=grid[x,y]*2;
+								grid[x+i,y]=0;
+								grid[x,y]=0;
+								break;
+							}
+					// lägg till så att newLine stoppar in i grid >:(
+				}
+			if(yDir!=0) ;
+		}
+		public static void Main(string[] args)
+
+		{
+			
+			addBlock(true, 10);
 			while(true) // gameloop
 			{
-
+				addBlock(true, 10);
+				display();
+				MoveLogic();
 			}
 		}
 	}
 }
+
+/*
+ 
+
+				while(hasMoved==false)
+				{
+					hasMoved=false;
+					for(int x=0; x<4; ++x)
+						for(int y=0; y<4; ++y)
+						{
+							if(move=='w')
+							{
+								if(y==0)
+									++y;
+								if(grid[x,y]==grid[x,y-1])
+								{
+									grid[x,y-1]*=2;
+									grid[x,y]=0;
+									hasMoved=true;
+								}
+								if(grid[x,y-1]==0 && grid[x,y]!=0)
+								{
+									grid[x,y-1]=grid[x,y];
+									grid[x,y]=0;
+									hasMoved=true;
+								}
+							}
+							else if(move=='a')
+							{
+								if(x==0)
+									++x;
+								if(grid[x,y]==grid[x-1,y])
+								{
+									grid[x-1,y]*=2;
+									grid[x,y]=0;
+									hasMoved=true;
+								}
+								if(grid[x-1,y]==0 && grid[x,y]!=0)
+								{
+									grid[x-1,y]=grid[x,y];
+									grid[x,y]=0;
+									hasMoved=true;
+								}
+							}
+							else if(move=='s' && y!=3)
+							{
+								if(grid[x,y]==grid[x,y+1])
+								{
+									grid[x,y+1]*=2;
+									grid[x,y]=0;
+									hasMoved=true;
+								}
+								if(grid[x,y+1]==0 && grid[x,y]!=0)
+								{
+									grid[x,y+1]=grid[x,y];
+									grid[x,y]=0;
+									hasMoved=true;
+								}
+							}
+							else if(move=='d' && x!=3)
+							{
+								if(grid[x,y]==grid[x+1,y])
+								{
+									grid[x+1,y]*=2;
+									grid[x,y]=0;
+									hasMoved=true;
+								}
+								if(grid[x+1,y]==0 && grid[x,y]!=0)
+								{
+									grid[x+1,y]=grid[x,y];
+									grid[x,y]=0;
+									hasMoved=true;
+								}
+							}
+						}
+					}
+
+*/
